d4841303ee4361b3743fc6a801ce73724fa749a7
Michael Brown <mikeb@360replays.com>
2022-01-25 14:08:08 -0800

    Adds total unique parts count for summary

--- Binner.StorageProvider.Sqlite/Binner.StorageProvider.Sqlite/SqliteStorageProvider.cs
+++ Binner.StorageProvider.Sqlite/Binner.StorageProvider.Sqlite/SqliteStorageProvider.cs
@@ -58,9 +58,16 @@ namespace Binner.StorageProvider.Sqlite
             };
         }
 
-        public async Task<long> GetPartsCountAsync(IUserContext userContext)
+        public async Task<long> GetUniquePartsCountAsync(IUserContext userContext)
         {
             var query = $"SELECT COUNT(*) FROM Parts WHERE (@UserId IS NULL OR UserId = @UserId);";
+            var result = await ExecuteScalarAsync<long>(query, new { UserId = userContext?.UserId });
+            return result;
+        }
+
+        public async Task<long> GetPartsCountAsync(IUserContext userContext)
+        {
+            var query = $"SELECT SUM(Quantity) FROM Parts WHERE (@UserId IS NULL OR UserId = @UserId);";
             var result = await ExecuteScalarAsync<long>(query, new { UserId = userContext?.UserId });
             return result;
         }
